7d1b2d56d13a030e1fc7a1a8f73bdb29404d55a2
Valentine <kerubovalentine1@gmail.com>
2023-06-26 20:43:55 +0300

    ongoing assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,25 @@
 class ItemsController < ApplicationController
 
-  def index
-    items = Item.all
-    render json: items, include: :user
+def index
+    if params[:user_id]
+      user = User.find_by(id: params[:user_id])
+      if user
+        items = user.items
+        render json: items, include: :user
+      else
+        render json: { error: "User not found" }, status: :not_found
+      end
+    else
+      render json: { error: "Missing user_id parameter" }, status: :bad_request
+    end
   end
+
+  #def show
+     #params[:user_id]
+      #user = User.find_by(id: params[:id])
+      #items = Item.user
+      #render json: items, include: :user
+   # end
+  #end
 
 end
